b99807578bd0e79cd7f9ff5571324ad66b496c04
John Smith <example@stanford.edu>
2013-07-10 23:35:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,11 +7,20 @@ app.get('/', function(request, response) {
 
 //var fs = require('fs');
 
-var buffer = new buffer ();
+//var buffer = new buffer ();
 
-response.send(fs.readFileSync("index.html", {encoding:"utf8"});
+//response.send(fs.readFileSync("index.html", {encoding:"utf8"});
 //response.send(fs.readFileSync("index.html", "utf8");
+
+ fs.readFile('index.html', {encoding:"utf8"}, function (err, data) {
+if (err) {
+response.send(err);
+}
+else {
+response.send(data);
+}
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
