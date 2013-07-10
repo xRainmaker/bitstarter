4e9a92c83b1a2f39572645bff6d7016620f0b824
John Smith <example@stanford.edu>
2013-07-10 02:09:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -8,9 +8,9 @@ app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new buffer ();
     response.send(buffer.tostring('utc 8', fs.readFilesSync("index.html")))});
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
